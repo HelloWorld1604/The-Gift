a9e6be4e352f049bad6829aa5520e32f3193aff0
Luong Trang <78288310+HelloWorld1604@users.noreply.github.com>
2025-07-09 17:08:39 +0900

    Create Astronaut.py

---
+++ Astronaut.py
@@ -0,0 +1,438 @@
+import os
+import time
+
+
+class A():
+	def core1():print("             ", end="")
+	def core2():print("             ", end="")
+	def core3():print("    ***      ", end="")
+	def core4():print("   ** **     ", end="")
+	def core5():print("  **   **    ", end="")
+	def core6():print(" *********   ", end="")
+	def core7():print("**       **  ", end="")
+
+class B():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("********   ", end="")
+	def core4():print("**     **  ", end="")
+	def core5():print("********   ", end="")
+	def core6():print("**     **  ", end="")
+	def core7():print("********   ", end="")
+
+class C():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("*********  ", end="")
+	def core4():print("**         ", end="")
+	def core5():print("**         ", end="")
+	def core6():print("**         ", end="")
+	def core7():print("*********  ", end="")
+
+class D():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("********   ", end="")
+	def core4():print("**     **  ", end="")
+	def core5():print("**     **  ", end="")
+	def core6():print("**     **  ", end="")
+	def core7():print("********   ", end="")
+
+class E():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("*********  ", end="")
+	def core4():print("**         ", end="")
+	def core5():print("*********  ", end="")
+	def core6():print("**         ", end="")
+	def core7():print("*********  ", end="")
+
+class F():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("*********  ", end="")
+	def core4():print("**         ", end="")
+	def core5():print("*********  ", end="")
+	def core6():print("**         ", end="")
+	def core7():print("**         ", end="")
+
+class G():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("*********  ", end="")
+	def core4():print("**         ", end="")
+	def core5():print("**   ****  ", end="")
+	def core6():print("**     **  ", end="")
+	def core7():print("*********  ", end="")
+
+class H():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("**     **  ", end="")
+	def core4():print("**     **  ", end="")
+	def core5():print("*********  ", end="")
+	def core6():print("**     **  ", end="")
+	def core7():print("**     **  ", end="")
+
+class I():
+	def core1():print("    ", end="")
+	def core2():print("    ", end="")
+	def core3():print("**  ", end="")
+	def core4():print("**  ", end="")
+	def core5():print("**  ", end="")
+	def core6():print("**  ", end="")
+	def core7():print("**  ", end="")
+
+class J():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("**         ", end="")
+	def core4():print("**         ", end="")
+	def core5():print("**         ", end="")
+	def core6():print("**      *  ", end="")
+	def core7():print(" *******   ", end="")
+
+class K():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("**    **   ", end="")
+	def core4():print("**   **    ", end="")
+	def core5():print("** **      ", end="")
+	def core6():print("**   **    ", end="")
+	def core7():print("**     **  ", end="")
+
+class L():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("**         ", end="")
+	def core4():print("**         ", end="")
+	def core5():print("**         ", end="")
+	def core6():print("**         ", end="")
+	def core7():print("*********  ", end="")
+
+class M():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("**    ***  ", end="")
+	def core4():print("** * * **  ", end="")
+	def core5():print("**  *  **  ", end="")
+	def core6():print("**     **  ", end="")
+	def core7():print("**     **  ", end="")
+
+class N():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("**     **  ", end="")
+	def core4():print("** *   **  ", end="")
+	def core5():print("**  *  **  ", end="")
+	def core6():print("**   * **  ", end="")
+	def core7():print("**     **  ", end="")
+
+class O():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("*********  ", end="")
+	def core4():print("**     **  ", end="")
+	def core5():print("**     **  ", end="")
+	def core6():print("**     **  ", end="")
+	def core7():print("*********  ", end="")
+
+class P():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("*********  ", end="")
+	def core4():print("**     **  ", end="")
+	def core5():print("*********  ", end="")
+	def core6():print("**         ", end="")
+	def core7():print("**         ", end="")
+
+class Q():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("*********  ", end="")
+	def core4():print("**     **  ", end="")
+	def core5():print("*********  ", end="")
+	def core6():print("       **  ", end="")
+	def core7():print("       **  ", end="")
+
+class R():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("********   ", end="")
+	def core4():print("**     **  ", end="")
+	def core5():print("*********  ", end="")
+	def core6():print("**    **   ", end="")
+	def core7():print("**     **  ", end="")
+
+class S():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("*********  ", end="")
+	def core4():print("**         ", end="")
+	def core5():print("*********  ", end="")
+	def core6():print("       **  ", end="")
+	def core7():print("*********  ", end="")
+
+class T():
+	def core1():print("          ", end="")
+	def core2():print("          ", end="")
+	def core3():print("********  ", end="")
+	def core4():print("   **     ", end="")
+	def core5():print("   **     ", end="")
+	def core6():print("   **     ", end="")
+	def core7():print("   **     ", end="")
+
+class U():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("**     **  ", end="")
+	def core4():print("**     **  ", end="")
+	def core5():print("**     **  ", end="")
+	def core6():print("**     **  ", end="")
+	def core7():print("*********  ", end="")
+
+class V():
+	def core1():print("             ", end="")
+	def core2():print("             ", end="")
+	def core3():print("**       **  ", end="")
+	def core4():print(" **     **   ", end="")
+	def core5():print("  **   **    ", end="")
+	def core6():print("   ** **     ", end="")
+	def core7():print("     *       ", end="")
+
+class W():
+	def core1():print("             ", end="")
+	def core2():print("             ", end="")
+	def core3():print("**       **  ", end="")
+	def core4():print("**       **  ", end="")
+	def core5():print("**   *   **  ", end="")
+	def core6():print(" ** * * **   ", end="")
+	def core7():print("   *   *     ", end="")
+
+class X():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("**     **  ", end="")
+	def core4():print(" **   **   ", end="")
+	def core5():print("    *      ", end="")
+	def core6():print(" **   **   ", end="")
+	def core7():print("**     **  ", end="")
+
+class Y():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("**     **  ", end="")
+	def core4():print(" **   **   ", end="")
+	def core5():print("    *      ", end="")
+	def core6():print("    *      ", end="")
+	def core7():print("    *      ", end="")
+
+class Z():
+	def core1():print("           ", end="")
+	def core2():print("           ", end="")
+	def core3():print("*********  ", end="")
+	def core4():print("      **   ", end="")
+	def core5():print("    **     ", end="")
+	def core6():print("  **       ", end="")
+	def core7():print("*********  ", end="")
+
+class one():
+	def core1():print("           ", end="")
+	def core2():print("   * *     ", end="")
+	def core3():print("**     **  ", end="")
+	def core4():print("**     **  ", end="")
+	def core5():print("**     **  ", end="")
+	def core6():print("**     **  ", end="")
+	def core7():print("*********  ", end="")
+
+class two():
+	def core1():print("     *     ", end="")
+	def core2():print("    *      ", end="")
+	def core3():print("*********  ", end="")
+	def core4():print("**         ", end="")
+	def core5():print("*********  ", end="")
+	def core6():print("**         ", end="")
+	def core7():print("*********  ", end="")
+
+def call(letter):
+	if letter == "A" or letter == "a":
+		return A
+	elif letter == "B" or letter == "b":
+		return B
+	elif letter == "C" or letter == "c":
+		return C
+	elif letter == "D" or letter == "d":
+		return D
+	elif letter == "E" or letter == "e":
+		return E
+	elif letter == "F" or letter == "f":
+		return F
+	elif letter == "G" or letter == "g":
+		return G
+	elif letter == "H" or letter == "h":
+		return H
+	elif letter == "I" or letter == "i":
+		return I
+	elif letter == "J" or letter == "j":
+		return J
+	elif letter == "K" or letter == "k":
+		return K
+	elif letter == "L" or letter == "l":
+		return L
+	elif letter == "M" or letter == "m":
+		return M
+	elif letter == "N" or letter == "n":
+		return N
+	elif letter == "O" or letter == "o":
+		return O
+	elif letter == "P" or letter == "p":
+		return P
+	elif letter == "Q" or letter == "q":
+		return Q
+	elif letter == "R" or letter == "r":
+		return R
+	elif letter == "S" or letter == "s":
+		return S
+	elif letter == "T" or letter == "t":
+		return T
+	elif letter == "U" or letter == "u":
+		return U
+	elif letter == "V" or letter == "v":
+		return V
+	elif letter == "W" or letter == "w":
+		return W
+	elif letter == "X" or letter == "x":
+		return X
+	elif letter == "Y" or letter == "y":
+		return Y
+	elif letter == "Z" or letter == "z":
+		return Z
+	elif letter == "1":
+		return one
+	elif letter == "2":
+		return two
+
+def run_cores(string):
+	for i in string:
+		if i == " ": continue
+		call(i).core1()
+	time.sleep(0.5)
+	print()
+	
+	for i in string:
+		if i == " ": continue
+		call(i).core2()
+	time.sleep(0.5)
+	print()
+	
+	for i in string:
+		if i == " ": continue
+		call(i).core3()
+	time.sleep(0.5)
+	print()
+	
+	for i in string:
+		if i == " ": continue
+		call(i).core4()
+	time.sleep(0.5)
+	print()
+	
+	for i in string:
+		if i == " ": continue
+		call(i).core5()
+	time.sleep(0.5)
+	print()
+	
+	for i in string:
+		if i == " ": continue
+		call(i).core6()
+	time.sleep(0.5)
+	print()
+	
+	for i in string:
+		if i == " ": continue
+		call(i).core7()
+	time.sleep(0.5)
+	print()
+
+#-------------------------------------------------------------------
+class Astronaut():
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+
+	def state_A():
+		print("      .....         ")
+		print("    .     . *       ")
+		print("   .   _  .  *      ")
+		print("   .   _  .  *      ")
+		print("    ._____. * _0.0_ ")
+		print("  __.      ._|  |  |")
+		print(" |  .      ._|__|__|")
+		print(" |  .      .        ")
+		print(" |__.______.        ")
+		print("      |  >          ")
+
+	def state_B():
+		print("      .....         ")
+		print("    .     . *       ")
+		print("   .   _  .  *      ")
+		print("   .   _  .  *      ")
+		print("    ._____. *       ")
+		print("  __.      .  _0.0_ ")
+		print(" |  .      ._|  |  |")
+		print(" |  .      ._|__|__|")
+		print(" |__.______.        ")
+		print("      |  |          ")
+
+	def state_C():
+		print("      .....         ")
+		print("    .     . *       ")
+		print("   .   _  .  *      ")
+		print("   .   _  .  *      ")
+		print("    ._____. * _0.0_ ")
+		print("  __.      ._|  |  |")
+		print(" |  .      ._|__|__|")
+		print(" |  .      .        ")
+		print(" |__.______.        ")
+		print("      >  |          ")
+
+	def state_D():
+		print("      .....    /       ")
+		print("    .     . *   -      ")
+		print("   .   _  .  * \       ")
+		print("   .   _  .  *         ")
+		print("    ._____. *        / ")
+		print("  __.      .        /  ")
+		print(" |  .      ._|  |  |   ")
+		print(" |  .      ._|__|__|   ")
+		print(" |__.______.           ")
+		print("      |  |             ")
+
+def i_will_call_an_Astronaut_bring_this_one_for_you():
+	for t in range(0,15):
+		Astronaut.state_B()
+		time.sleep(0.1)
+		os.system("cls")
+
+		Astronaut.state_A()
+		time.sleep(0.1)
+		os.system("cls")
+
+		Astronaut.state_B()
+		time.sleep(0.1)
+		os.system("cls")
+
+		Astronaut.state_C()
+		time.sleep(0.1)
+		os.system("cls")
+
+
+	Astronaut.state_D()
+	time.sleep(0.5)
+	os.system("cls")
+
+#-------------------------------------------------------------------
+i_will_call_an_Astronaut_bring_this_one_for_you()
+run_cores("Happy")
+run_cores("Birthday")
